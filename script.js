220c58d8c72ce85e01a035e2576312dda1c0e659
Silviu Miron <silviu_miron_90@outlook.com>
2026-01-03 11:41:05 +0000

    feat:Inplement functionality for loan request button

--- script.js
+++ script.js
@@ -173,6 +173,21 @@ btnTransfer.addEventListener('click', function (e) {
     updateUI(currentAccount);
   }
 });
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movments
+    currentAccount.movements.push(amount);
+    // UPDATE UI
+    updateUI(currentAccount);
+    inputLoanAmount.value = '';
+  }
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
