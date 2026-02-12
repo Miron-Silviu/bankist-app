e4c078cdcd1dd5cdc3ad11f3bcfcb66a1e951d36
Silviu Miron <silviu_miron_90@outlook.com>
2026-02-12 08:54:49 +0000

    fix: Fix date format.

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ const account1 = {
     '2026-04-01T10:17:24.185Z',
     '2026-05-08T14:11:59.604Z',
     '2026-05-26T17:01:17.194Z',
-    '2026-05-28T23:36:17.929Z',
-    '2026-02-01T10:51:36.790Z',
+    '2026-02-11T23:36:17.929Z',
+    '2026-02-12T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -80,12 +80,11 @@ const formatMovementDate = function (date) {
   if (daysPassed === 0) return 'Today';
   if (daysPassed === 1) return 'Yesterday';
   if (daysPassed <= 7) return `${daysPassed} day ago`;
-  else {
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-    return `${day}/${month}/${year},`;
-  }
+
+  const day = `${date.getDate()}`.padStart(2, 0);
+  const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  const year = date.getFullYear();
+  return `${day}/${month}/${year},`;
 };
 
 const displayMovements = function (acc, sort = false) {
